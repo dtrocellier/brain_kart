67d98f949692c2147777ce05e7721ff206ee1728
juliettemnr <meunierjuliette62@gmail.com>
2024-02-10 19:20:34 +0100

    Frequency, turn, stop and save csv

---
+++ OpenVibe/save_csv.py
@@ -0,0 +1,46 @@
+# we use csv to the save the perfomance
+
+import time
+import csv
+# let's define a new box class that inherits from OVBox
+class MyOVBox(OVBox):
+   out = []
+   def __init__(self):
+      OVBox.__init__(self)
+   # we add a new member to save the signal header information we will receive
+      self.signalHeader = None
+
+
+   def initialize(self):
+       print(self.setting)
+       self.prenom = self.setting['pseudo']
+       self.event =  self.setting['event']
+
+
+   
+   def uninitialize(self):
+
+
+      score = self.getCurrentTime() 
+      fichier_gdf = f"{self.prenom}_{self.event}_{score}.gdf"
+      nouvelle_ligne = [self.prenom, self.event, score, fichier_gdf ]
+
+      nom_fichier = "bin/performances.csv"
+
+      #Ouvrir le fichier CSV en mode écriture
+      with open(nom_fichier, mode='a', newline='') as file:
+         # Créer un objet writer
+         writer = csv.writer(file)
+
+      #Ajouter la nouvelle ligne au fichier CSV
+         writer.writerow(nouvelle_ligne)
+
+
+
+
+     
+
+# Finally, we notify openvibe that the box instance 'box' is now an instance of MyOVBox.
+# Don't forget that step !!
+box = MyOVBox()
+#%%
